6f6cb7381d1f4c615a114991fe428e253fd58dea
yosukeueda33 <iwalkedalongthewalllikaeninjaa@gmail.com>
2025-11-22 16:46:25 +0900

    Add basic functions.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # movMidi
+
+# Command for mpv.
+
+```bash
+mpv mov.mp4 --input-ipc-server=/tmp/mpvsocket --keep-open=yes --pause=yes --no-border
+```
+
+Resizing window is Alt+F8 for this on Linux.
+
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,128 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import Lib
+module Main where
+
+import Sound.PortMidi (PMEvent(..), DeviceInfo(..),
+                       initialize, countDevices, getDeviceInfo,
+                       openInput, readEvents, setFilter,
+                       filterSysex, decodeMsg, status, data1, data2,
+                       input)
+import Control.Monad (forever)
+import Data.Bits ((.&.))
+import qualified Data.ByteString.Char8  as BS
+import           System.IO
+import           System.Process
+import           Network.Socket
+                  (Socket, SockAddr(SockAddrUnix), socket, connect
+                  , Family(AF_UNIX), SocketType(Stream), socketToHandle)
+import           System.Directory
+import           System.Posix.IO
+import           System.Posix.Types
+import           Control.Concurrent (threadDelay, forkIO)
+import Control.Exception (catch, IOException)
+
+enumerateDevices :: IO [DeviceInfo]
+enumerateDevices = do
+  count <- countDevices
+  mapM getDeviceInfo [0 .. count - 1]
+
+waitForSocket :: FilePath -> IO ()
+waitForSocket path = do
+  exists <- doesFileExist path
+  if exists then pure ()
+            else do
+              threadDelay 200000
+              waitForSocket path
+
+connectToMPV' :: FilePath -> IO Handle
+connectToMPV' path = do
+  sock <- socket AF_UNIX Stream 0 :: IO Socket
+  connect sock (SockAddrUnix path)
+  socketToHandle sock WriteMode
+
+connectToMPV :: FilePath -> IO Handle
+connectToMPV path = do
+  sock <- socket AF_UNIX Stream 0 :: IO Socket
+  connect sock (SockAddrUnix path)
+  h <- socketToHandle sock ReadWriteMode
+  hSetBuffering h LineBuffering
+
+  -- Reader thread: consume mpv replies so its send buffer doesn't fill
+  _ <- forkIO $ forever $ do
+         -- mpv sends JSON lines; if mpv quits, this will throw
+         BS.hGetLine h `catch` \(_ :: IOException) -> pure BS.empty
+  return h
+
+sendMPV' :: Handle -> String -> IO ()
+sendMPV' h msg = do
+    BS.hPutStrLn h $ BS.pack msg
+    hFlush h
+    return ()
+
+sendMPV :: Handle -> String -> IO ()
+sendMPV h msg =
+  (BS.hPutStrLn h (BS.pack msg) >> hFlush h)
+    `catch` \e -> do
+      let _ = e :: IOException
+      putStrLn $ "mpv write failed: " ++ show e
 
 main :: IO ()
-main = someFunc
+main = do
+  -- Connect to mpv socket
+  putStrLn "Waiting for mpv socket..."
+  waitForSocket "/tmp/mpvsocket"
+  putStrLn "Connecting to mpv..."
+  sockH <- connectToMPV "/tmp/mpvsocket" :: IO Handle
+  putStrLn "Connected to mpv."
+
+  -- Initialize PortMIDI
+  initialize
+
+  -- List devices
+  devs <- enumerateDevices
+  mapM_ (\(id, info) -> putStrLn $ show id ++ ": " ++ show (name info))
+    . filter (input . snd) $ zip [0..] devs
+
+  -- devID <- readLn
+  let devID = 3
+
+  result <- openInput devID
+  case result of
+    Left err -> putStrLn ("Error opening MIDI: " ++ show err)
+    Right stream -> do
+      putStrLn "Listening for MIDI..."
+
+      setFilter stream filterSysex
+
+      forever $ do
+        evs <- readEvents stream
+        case evs of
+          Right events -> mapM_ (handleEvent sockH) events
+          Left _       -> pure ()
+
+playAt:: Handle -> String -> IO ()
+playAt h timeStr = do
+  sendMPV h  ("{\"command\": [\"seek\", \"" ++ timeStr ++ "\", \"absolute\"] }")
+  sendMPV h  "{\"command\": [\"set_property\", \"pause\", false] }"
+  return ()
+
+handleEvent :: Handle -> PMEvent -> IO ()
+handleEvent sockH (PMEvent clMsg _) =
+  let 
+      msg = decodeMsg clMsg
+      stat = status msg
+      d1 = data1 msg
+      d2 = data2 msg
+      chan = stat .&. 0x0F
+   in case stat .&. 0xF0 of
+        0x90 -> do   -- NoteOn
+          if d2 == 0
+            then putStrLn $ "chan:" ++ show chan
+                            ++ " Note Off (via velocity 0): " ++ show d1
+            else do
+              putStrLn $ "chan:" ++ show chan ++ " Note On: " ++ show d1
+              playAt sockH "00:00:01.5"
+
+        0x80 -> putStrLn $ "chan:" ++ show chan ++ " Note Off: " ++ show d1
+        _    -> pure ()
